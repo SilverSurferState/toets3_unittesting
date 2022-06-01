01936fe7beedc92a41b0175c523b9f86fa1696c0
beto <beto@asuvorm.be>
2022-06-01 18:49:22 +0200

    created second test to ensure array is filled and constants return string

--- src/test/java/extoets3_mastermind/MastermindTest.java
+++ src/test/java/extoets3_mastermind/MastermindTest.java
@@ -2,8 +2,7 @@ package extoets3_mastermind;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MastermindTest {
 
@@ -13,4 +12,20 @@ class MastermindTest {
         Mastermind mastermind = new Mastermind( 5 );
         assertEquals(5, mastermind.getArrOfStrings().length);
     }
+
+    @Test
+    public void testNewMastermindFilled(){
+        Mastermind mastermind = new Mastermind( 7 );
+        String[] arr = mastermind.getArrOfStrings();
+        arr[0] = Mastermind.RED;
+        arr[1] = Mastermind.GREEN;
+        arr[2] = Mastermind.BLUE;
+        arr[3] = Mastermind.ORANGE;
+        arr[4] = Mastermind.BLACK;
+        assertEquals(7, mastermind.getArrOfStrings().length);
+        assertEquals("orange", arr[3]);
+    }
+
+
+
 }
