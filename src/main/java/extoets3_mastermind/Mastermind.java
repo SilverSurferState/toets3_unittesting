20a663158c435257be266f93eeb16439ec698aa4
beto <beto@asuvorm.be>
2022-06-01 18:24:49 +0200

    Class created with constants

---
+++ src/main/java/extoets3_mastermind/Mastermind.java
@@ -0,0 +1,16 @@
+package extoets3_mastermind;
+
+public class Mastermind {
+    public static final String RED = "red";
+    public static final String GREEN = "green";
+    public static final String BLUE = "blue";
+    public static final String ORANGE = "orange";
+    public static final String BLACK = "black";
+    public static final String WHITE = "white";
+
+    public static final int RESULT_BAD = 0;
+    public static final int RESULT_ALMOST = 1;
+    public static final int RESULT_OK = 2;
+
+
+}
